280fc49ff151f764cb901a96260c48a528839c8a
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-06-21 22:48:20 +0300

    Removing extraneous jar

--- src/il/technion/cs236369/webserver/TSPTranslator.java
+++ src/il/technion/cs236369/webserver/TSPTranslator.java
@@ -1,13 +1,13 @@
 package il.technion.cs236369.webserver;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.StringWriter;
 import java.util.Map;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.http.entity.FileEntity;
 
 public class TSPTranslator {
@@ -22,22 +22,23 @@ public class TSPTranslator {
     		File file = null;
     		FileEntity body = new FileEntity(file);
             InputStream bodyContent = body.getContent();
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(bodyContent, writer, "UTF-8");
-            String bodyString = writer.toString();
+            BufferedReader br = new BufferedReader(new InputStreamReader(bodyContent));
             String compiledBody = "";
-            for (int i = 0; i < bodyString.length(); i++){
-                char c = bodyString.charAt(i);  
-                if (c == '<' && bodyString.charAt(i+1) == '?') {
-                	out.println(compiledBody.replace("\"", "\\\"").replace("\\", "\\\\"));
-                	compiledBody = "";
-                }
-                else if (c == '?' && bodyString.charAt(i+1) == '>') {
-                	out.println(compiledBody);
-                	compiledBody = "";
-                }
-                compiledBody += c;
+            while (br.ready()) {
+                String bodyString = br.readLine();
+                for (int i = 0; i < bodyString.length(); i++){
+                    char c = bodyString.charAt(i);  
+                    if (c == '<' && bodyString.charAt(i+1) == '?') {
+                    	out.println(compiledBody.replace("\"", "\\\"").replace("\\", "\\\\"));
+                    	compiledBody = "";
+                    }
+                    else if (c == '?' && bodyString.charAt(i+1) == '>') {
+                    	out.println(compiledBody);
+                    	compiledBody = "";
+                    }
+                    compiledBody += c;
 
+                }
             }
         } catch (IOException e) {
         	e.printStackTrace();
