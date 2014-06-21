e50162c02109b0f324dfdb733950972f487a8db5
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-06-21 15:34:23 +0300

    Started SessionManager

--- src/il/technion/cs236369/webserver/SessionManager.java
+++ src/il/technion/cs236369/webserver/SessionManager.java
@@ -1,5 +1,20 @@
 package il.technion.cs236369.webserver;
 
+import java.util.HashMap;
+
 public class SessionManager {
+	private HashMap<String, Session> sessionMap = new HashMap<String, Session>(); 
+	
+	public SessionManager() {
+		
+	}
+	
+	public void addSession(String accessor, Session sToadd) {
+		sessionMap.put(accessor, sToadd);
+	}
+	
+	public Session getSession(String accessor) {
+		return sessionMap.get(accessor);
+	}
 
 }
