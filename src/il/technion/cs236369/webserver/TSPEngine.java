64a5ace48780a4bf43ed15d72c8871b4b887bc83
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-06-22 22:16:40 +0300

    Added method for David to call

--- src/il/technion/cs236369/webserver/TSPEngine.java
+++ src/il/technion/cs236369/webserver/TSPEngine.java
@@ -1,14 +1,10 @@
 package il.technion.cs236369.webserver;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintStream;
-import java.net.URLDecoder;
-import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.tools.JavaCompiler;
@@ -16,39 +12,19 @@ import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 
-import org.apache.http.Header;
-import org.apache.http.HeaderIterator;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.HttpRequestHandler;
-
-public class TSPEngine implements HttpRequestHandler {
+public class TSPEngine {
 	private String baseDir;
 	private String classToDynamicallyLoad;
-	private HashSet<String> typeHandlerExtensions;
-    public final static int BUFSIZE = 8 * 1024;
-	Properties properties;
-	private String mimeType;
 	private final JavaCompiler compiler;
 	private final StandardJavaFileManager manager;
 	private String jre_path;
 	private int counter = 0;
 	private SessionManager sessionManager;
-	private int timeout;
 	
 	public TSPEngine(Properties p) {
-		properties = p;
 		jre_path = p.getProperty("jre_path");
 		baseDir = p.getProperty("baseDir");
-		timeout = Integer.parseInt(p.getProperty("timeout"));
 		classToDynamicallyLoad = p.getProperty("classToDynamicallyLoad");
-		mimeType = p.getProperty("mimeType");
 		compiler = ToolProvider.getSystemJavaCompiler();
 		if (compiler == null)
 			throw new RuntimeException("compiler not found");
@@ -58,86 +34,36 @@ public class TSPEngine implements HttpRequestHandler {
 		sessionManager = new SessionManager();
 	}
 	
-	@Override
-	public void handle(HttpRequest request, HttpResponse response, HttpContext arg2)
-			throws HttpException, IOException {
-		String uri = request.getRequestLine().getUri();
+	/**
+	 * David, use this method to use the TSP handler.
+	 * @param request
+	 * @param params
+	 * @param session
+	 * @return
+	 */
+	public PrintStream handleTSP(Request request, Map<String, String> params, Session session) {
+		//String uri = request.getRequestedPath();
 		try {
-			final File file = new File(baseDir, URLDecoder.decode(uri, "UTF-8"));
-			if (!file.exists()) {
-				response.setStatusCode(HttpStatus.SC_NOT_FOUND);
-				StringEntity entity = new StringEntity(
-						"<html><body><h1>File" + file.getPath() +
-						" not found</h1></body></html>",
-						ContentType.create("text/html", "UTF-8"));
-				response.setEntity(entity);
-				System.out.println("File " + file.getPath() + " not found");
-			} else if (!file.canRead() || uri.contains("..")) {
-				response.setStatusCode(HttpStatus.SC_FORBIDDEN);
-				StringEntity entity = new StringEntity(
-						"<html><body><h1>Access denied</h1></body></html>",
-						ContentType.create("text/html", "UTF-8"));
-				response.setEntity(entity);
-				System.out.println("Cannot read file " + file.getPath());
-			}
-			else if (file.isDirectory()) {
-				File[] listOfFiles = file.listFiles();
-				String htmlToReturn = "<html><body><ul>";
-			    for (int i = 0; i < listOfFiles.length; i++) {
-			    	htmlToReturn += "<li>" + listOfFiles[i].getName() + "</li>";
-			    }
-				htmlToReturn += "</ul></body></html>";
-				StringEntity entity = new StringEntity(
-						htmlToReturn,
-						ContentType.create("text/html", "UTF-8"));
-				response.setEntity(entity);
-				System.out.println("File is a directory " + file.getPath());
-			}
-			else {
-				String[] beenSplit = uri.split(".");
-				String extension = beenSplit[beenSplit.length - 1];
-				int questionMarkIndex = extension.indexOf("?");
-				int poundSignIndex = extension.indexOf("#");
-				if (questionMarkIndex != -1) {
-					extension = extension.substring(0, questionMarkIndex);
-				}
-				else if (poundSignIndex != -1){
-					extension = extension.substring(0, poundSignIndex);
-				}
-				if (typeHandlerExtensions.contains(extension)) {
-	                response.setStatusCode(HttpStatus.SC_OK);
-	                File toReturn = new File("toReturn");
-	                compile(request, response, toReturn);
-	                response.setEntity(new FileEntity(toReturn));
-	                System.out.println("Serving file " + file.getPath());
-				}
-				else {
-	                response.setStatusCode(HttpStatus.SC_OK);
-	                FileEntity body = new FileEntity(file, ContentType.create(mimeType, (Charset) null));
-	                response.setEntity(body);
-	                System.out.println("Serving file " + file.getPath());
-				}
-			}
+			//final File file = new File(baseDir, URLDecoder.decode(uri, "UTF-8"));
+			File toReturn = new File("./toReturn");
+			return compile(request, toReturn, session, params);
 		}catch (Exception e) {
 			e.printStackTrace();
-		}		
+		}
+		return null;		
 	}
 	
-	private void compile(HttpRequest req, HttpResponse res, File fToReturn) throws Exception {
+	private PrintStream compile(Request req, File fToReturn, Session session,
+			Map<String, String> params) throws Exception {
 		String fs = File.separatorChar + "";
 		Class<?> a = compileAndLoad(classToDynamicallyLoad.replace(".", fs), classToDynamicallyLoad);
 		Object o = a.newInstance();
 		ITSPTranslator t = (ITSPTranslator) o;
 		PrintStream printStreamToUse = new PrintStream(fToReturn);
-        HeaderIterator hIterator = req.headerIterator();
-        HashMap<String, String> params = new HashMap<String, String>();
-        while (hIterator.hasNext()) {
-        	Header nextEle = hIterator.nextHeader();
-        	params.put(nextEle.getName(), nextEle.getValue());
-        }
 		t.translate(printStreamToUse, params,
-				new Session(res, timeout), sessionManager);
+				session, sessionManager);
 		t = null;
+		return printStreamToUse;
 	}
 	
 	
@@ -146,6 +72,20 @@ public class TSPEngine implements HttpRequestHandler {
 		Iterable<? extends JavaFileObject> units = manager.getJavaFileObjects(srcPath);
 		List<String> optionsList = Arrays.asList(new String[] { "-d", "bin" });
 		optionsList.addAll(Arrays.asList("-classpath",jre_path));
+		
+		/* For example: this is essentially what units are:
+		   StringWriter writer = new StringWriter();
+		    PrintWriter out = new PrintWriter(writer);
+		    out.println("public class HelloWorld {");
+		    out.println("  public static void main(String args[]) {");
+		    out.println("    System.out.println(\"This is in another java file\");");    
+		    out.println("  }");
+		    out.println("}");
+		    out.close();
+		    JavaFileObject file = new JavaSourceFromString("HelloWorld", writer.toString());
+		    Iterable<? extends JavaFileObject> units = Arrays.asList(file);
+		    */
+		
 		Boolean status = compiler.getTask(null, manager, null, optionsList, null, units)
 				.call();
 		if (status == null || !status.booleanValue()) {
--- src/il/technion/cs236369/webserver/TSPTranslator.java
+++ src/il/technion/cs236369/webserver/TSPTranslator.java
@@ -20,47 +20,44 @@ public class TSPTranslator implements ITSPTranslator{
 	//not this class.  Either way, the following code is not complete.
 	public void translate(PrintStream out, Map<String, String> params,
 			Session session, SessionManager sessionManager) {
-        try {
-        	File fileToRead;
-        	if (params.get("GET") != null) {
-        		fileToRead = new File(params.get("GET"));
-        	}
-        	else if (params.get("POST") != null) {
-        		fileToRead = new File(params.get("POST"));
-        	}
-        	else {
-        		throw new IOException("There is no given file path.");
-        	}
-        	int curr = 0;
-    		FileEntity body = new FileEntity(fileToRead);
-            InputStream bodyContent = body.getContent();
-            BufferedReader br = new BufferedReader(new InputStreamReader(bodyContent));
-            boolean lineEndsWithScriptlet = false;
-            while (br.ready()) {
-                String lineString = br.readLine();
-                int scriptletStart = lineString.indexOf("<?");
-                int scriptletEnd = lineString.indexOf("?>");
-                if (lineEndsWithScriptlet) {
-                	params.get(curr);
-                	curr++;
-                }
-                else if (scriptletStart != -1) {
-                	out.print(lineString.replace("\"", "\\\"").replace("\\", "\\\\"));
-                }
-                else {
-                	params.get(curr);
-                	curr++;
-                	if (scriptletEnd != -1) {
-                    	out.print(lineString.substring(scriptletStart, scriptletEnd));
-                    	out.print(lineString.substring(scriptletEnd));
-                	}
-                	else {
-                		out.print(lineString.substring(scriptletStart));
-                	}
-                }
+		//As suspected, this is all useless:
+//        try {
+//        	File fileToRead;
+//        	if (params.get("GET") != null) {
+//        		fileToRead = new File(params.get("GET"));
+//        	}
+//        	else if (params.get("POST") != null) {
+//        		fileToRead = new File(params.get("POST"));
+//        	}
+//        	else {
+//        		throw new IOException("There is no given file path.");
+//        	}
+//        	int curr = 0;
+//    		FileEntity body = new FileEntity(fileToRead);
+//            InputStream bodyContent = body.getContent();
+//            BufferedReader br = new BufferedReader(new InputStreamReader(bodyContent));
+//            boolean lineEndsWithScriptlet = false;
+//            while (br.ready()) {
+//                String lineString = br.readLine();
+//                int scriptletStart = lineString.indexOf("<?");
+//                int scriptletEnd = lineString.indexOf("?>");
+//                if (lineEndsWithScriptlet) {
+//                	params.get(curr);
+//                	curr++;
+//                }
+//                else if (scriptletStart != -1) {
+//                	out.print(lineString.replace("\"", "\\\"").replace("\\", "\\\\"));
+//                }
+//                else {
+//                	params.get(curr);
+//                	curr++;
+//                	if (scriptletEnd != -1) {
+//                    	out.print(lineString.substring(scriptletStart, scriptletEnd));
+//                    	out.print(lineString.substring(scriptletEnd));
+//                	}
+//                	else {
+//                		out.print(lineString.substring(scriptletStart));
+//                	}
+//                }
             }
-        } catch (IOException e) {
-        	e.printStackTrace();
-        }
-	}
 }
